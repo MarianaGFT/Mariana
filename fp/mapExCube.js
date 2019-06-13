55bc3510f022e9448f5f18b0a18ad82afd011d28
Fajardo Toscano Mariana Guadalupe on Dcomplab <mfajardo1@ucol.mx>
2019-06-12 19:36:42 -0500

    borrando la carpeta 3p

--- fp/mapExCube.js
+++
@@ -1,9 +0,0 @@
-//doubles using Map : no forEach : no k
-let mapExCube=function(list){
-    return list.map(function(number){
-        return number*number*number;
-    });
-};
-
-let cube=[5,4,3,2,1];
-console.log(mapExCube(cube));//verify result
--- fp/mapExSubt3.js
+++
@@ -1,9 +0,0 @@
-//doubles using Map : no forEach : no k
-let mapExSubt3=function(list){
-    return list.map(function(number){
-        return number-3;
-    });
-};
-
-let subt=[5,4,3,2,1];
-console.log(mapExSubt3(subt));//verify result
